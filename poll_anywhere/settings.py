d4449b7b4000bccbc47fa225ada5ccaa87c543e0
Sahil <sahils.1997.s@gmail.com>
2020-09-05 13:00:41 +0530

    Add apps in settings

--- poll_anywhere/settings.py
+++ poll_anywhere/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'user.apps.UserConfig',
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
