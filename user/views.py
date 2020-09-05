a2c718dd5ff6ac07e28a6f329fffe31c016a2d24
Sahil <sahils.1997.s@gmail.com>
2020-09-05 18:38:36 +0530

    some views added

--- user/views.py
+++ user/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserChangeForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
