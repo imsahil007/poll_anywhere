38415bc69dc9ecc94eaa4a0418f8638a43b5ac8b
imsahil007 <sahils.1997.s@gmail.com>
2020-09-08 10:14:14 +0530

    profile templates added

--- user/models.py
+++ user/models.py
@@ -7,8 +7,8 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default = 'default.jpg', upload_to='profile_pics')
 
-    def save(self):
-        super().save()
+    def save(self,*args, **kwargs):
+        super().save(*args, **kwargs)
         img = Image.open(self.image.path)
         output_size = (300,300)
         if img.height > 300 or img.width > 300:
