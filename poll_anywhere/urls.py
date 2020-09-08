71f43d558b566e503a1170d65a1c86c02fcfb42f
imsahil007 <sahils.1997.s@gmail.com>
2020-09-08 06:15:04 +0530

    Url for project added

--- poll_anywhere/urls.py
+++ poll_anywhere/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from user import views as user_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('polls.urls'), name='polls')
+    path('',include('polls.urls')),
+    path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'),name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'),name='logout'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='user/password_reset.html'),name='password-reset'),
+    path('password-reset/done', auth_views.PasswordResetDoneView.as_view(template_name='user/password_reset_done.html'),name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html'),name='password_reset_confirm'),
 ]
