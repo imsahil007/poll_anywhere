e7762fe3c9275884d36988c64ec87ed9ba0035f4
imsahil007 <sahils.1997.s@gmail.com>
2020-09-08 07:16:47 +0530

    User templates login logout register

--- poll_anywhere/settings.py
+++ poll_anywhere/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'user.apps.UserConfig',
     'polls.apps.PollsConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
