86326e071ad3d6834cde9189dffbc8015534b27d
imsahil007 <sahils.1997.s@gmail.com>
2020-09-10 14:48:59 +0530

    Complete deployment

--- poll_anywhere/urls.py
+++ poll_anywhere/urls.py
@@ -14,8 +14,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from user import views as user_views
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import reverse_lazy
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('polls.urls')),
+    path('register/', user_views.register, name='register'),
+    path('accounts/profile/', user_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'),name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'),name='logout'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='user/password_reset.html'),name='password-reset'),
+    path('password-reset/done', auth_views.PasswordResetDoneView.as_view(template_name='user/password_reset_done.html'),name='password_reset_done'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='user/password_reset_complete.html'),name='password_reset_complete'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html'),name='password_reset_confirm'),
 ]
+
+if settings.DEBUG:
+    urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
