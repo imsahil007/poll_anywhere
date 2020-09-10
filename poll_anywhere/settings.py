09d6eda937cdc1430e31eb037348e0f5ac9eb8d2
imsahil007 <sahils.1997.s@gmail.com>
2020-09-10 08:15:00 +0530

    Environment variables setted

--- poll_anywhere/settings.py
+++ poll_anywhere/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=#2+=#8o+sg976p8h328@+bh%$%)ify_x1j^^hu)6q#ze@(pz+'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -136,11 +136,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER =os.environ.get('EMAIL')
-# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER =os.environ.get('EMAIL')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-# EMAIL_HOST_PASSWORD =  os.environ.get('EMAIL_PASS')
+EMAIL_HOST_PASSWORD =  os.environ.get('PASSWORD')
