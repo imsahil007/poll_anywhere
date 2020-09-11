60133bcb7a701c810a9ac730e0b127df47a86ab8
imsahil007 <sahils.1997.s@gmail.com>
2020-09-11 15:17:17 +0530

    Change in static files

--- poll_anywhere/settings.py
+++ poll_anywhere/settings.py
@@ -133,8 +133,7 @@ USE_TZ = True
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'pollcreator/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
